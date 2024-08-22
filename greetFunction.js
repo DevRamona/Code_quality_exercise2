ebface66e095030e7120e98842c793cd531a4605
DevRamona <r.ingabire@alustudent.com>
2024-08-22 08:46:23 +0200

    a greeting function

---
+++ greetFunction.js
@@ -0,0 +1,11 @@
+// function greet(name) {
+//     if (name) {
+//       return "Hello, " + name + "!";
+//     } else {
+//       return "Hello!";
+//     }
+//   }
+
+const greeting = (name) => {
+    return (name) ? `Hello, ${name}!` : "Hello!"
+}
